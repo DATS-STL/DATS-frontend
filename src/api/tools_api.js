504b5d2992a977eabe48e92c17b3f0c8119cac1d
Tom Gittemeier <gatormeier@yahoo.com>
2020-07-28 18:35:18 -0500

    front end updates

--- src/api/tools_api.js
+++ src/api/tools_api.js
@@ -1,5 +1,25 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL: 'http://127.0.0.1:5000'
+
+const client = axios.create({
+    baseURL: 'http://127.0.0.1:5000',
+    json: true
+
 });
+
+class ToolsApiClient {
+    getWelcomeDan() {
+        return this.perform('get', '/welcome_dan');
+    }
+
+    async  perform (method, resource) {
+        return client({
+            method,
+            url: resource
+        }).then(resp => {
+            return resp.data ? resp.data : [];
+        })
+    }
+}
+
+export default ToolsApiClient;
